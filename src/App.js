c18b63de212cdceeaa6f6f6fc1fc5a7fae7ad660
Maxel <gigaabait@yandex.ru>
2021-03-25 19:47:09 +0400

    lab ready

--- src/App.js
+++ src/App.js
@@ -1,47 +1,97 @@
 import React, { useState, useEffect } from 'react';
-import { getList } from './api';
+import { getList, getFiles, postEmp, postFile } from './api';
 
 import './App.css';
 
 function App() {
-  const [list, setList] = useState([]);
-  const [inputValue, setInputValue] = useState('')
+  const [peopleList, setPeopleList] = useState([]);
+  const [fileList, setFileList] = useState([]);
+  const [inputValue, setInputValue] = useState({
+    name: '',
+    address: '',
+    email: '',
+    phone: ''
+  })
   const [fileValue, setFileValue] = useState();
 
+  //Получение данных при загрузке
     useEffect(() => {
         getList().then(response => response.json())
-            .then(json => setList(json.slice(0, 10)))
+            .then(json => setPeopleList(json))
+        getFiles().then(response => response.json())
+            .then(json => setFileList(json))
     }, [])
 
-    const listItems = list.map((item) => {
-        return <p key={item.id}>{item.title}</p>
-    })
 
-    const handleChangeInputValue = (event) => {
-      setInputValue(event.target.value);
+    //Обработка вводимых данных о человеке
+    const handleChangeInputValue = (event, type) => {
+      if(type === 'name') {
+        setInputValue({
+          ...inputValue,
+          name: event.target.value 
+        });
+      }
+      if(type === 'address') {
+        setInputValue({
+          ...inputValue,
+          address: event.target.value 
+        });
+      }
+      if(type === 'email') {
+        setInputValue({
+          ...inputValue,
+          email: event.target.value 
+        });
+      }
+      if(type === 'phone') {
+        setInputValue({
+          ...inputValue,
+          phone: event.target.value 
+        });
+      }
     }
 
+    //Обработка вводимого файла
     const handleChangeFileValue = (event) => {
-
       setFileValue({
         document: event.target.files[0],
         name: event.target.files[0].name
       })
     }
 
-    if(fileValue) {
-      const formData = new FormData();
-      formData.append('document', fileValue.document)
-      formData.append('name', fileValue.name)
-      console.log(formData.get('name'));
+    const handleSendInputData = () => {
+      if(inputValue.name) {
+        postEmp(inputValue).then((response) => console.log(response))
+      }
     }
-    
+
+    const handleSendFile = () => {
+      if (fileValue) {
+        const formData = new FormData();
+        formData.append('data', fileValue.document)
+        formData.append('file_name', fileValue.name)
+        formData.append('extencion', `.${fileValue.name.split('.').pop()}`)
+        postFile(formData)
+      } 
+    }
+
+    //Список с людьми
+    const listItems = peopleList.map((item) => {
+      return (
+        <div key={item.id} className='item'>
+          <p>Имя: {item.name}</p>
+          <p>Почта: {item.email}</p>
+          <p>Адрес: {item.address}</p>
+          <p>Телефон: {item.phone}</p>
+        </div>
+      )      
+  })
 
     return (
       <div className='app'>
         <div className="content">
           <div className="list">
-            <h2>Текст</h2>
+            <h2>Люди</h2>
                {listItems}
           </div>
           <div className="images">
@@ -52,17 +102,29 @@ function App() {
           <div className="form-container">
           <form className="form">
             <label>
-              <h6>Введите текст</h6>
-              <input className="input" type="text" value={inputValue} onChange={handleChangeInputValue}/>
+              <h6>Введите имя</h6>
+              <input className="input" type="text" value={inputValue.name} onChange={(event) => handleChangeInputValue(event, 'name')}/>
             </label>
-            <button className="button" type="button">
+            <label>
+              <h6>Введите адрес</h6>
+              <input className="input" type="text" value={inputValue.address} onChange={(event) => handleChangeInputValue(event, 'address')}/>
+            </label>
+            <label>
+              <h6>Введите почту</h6>
+              <input className="input" type="email" value={inputValue.email} onChange={(event) => handleChangeInputValue(event, 'email')}/>
+            </label>
+            <label>
+              <h6>Введите телефон</h6>
+              <input className="input" type="text" value={inputValue.phone} onChange={(event) => handleChangeInputValue(event, 'phone')}/>
+            </label>
+            <button className="button" type="button" onClick={handleSendInputData}>
               Отправить текст
             </button>
           </form>
           <form className="form">
             <h6>Отправьте файл</h6>
             <input className='input' type='file' onChange={handleChangeFileValue}/>
-            <button className="button" type="button" >
+            <button className="button" type="button" onClick={handleSendFile}>
               Отправить файл
             </button>
           </form>
--- src/api/index.js
+++ src/api/index.js
@@ -1,1 +1,12 @@
-export const getList = () => fetch('https://jsonplaceholder.typicode.com/todos')
+const appBase =  'https://lab4and5.herokuapp.com/';
+
+export const getList = () => fetch(`${appBase}emp`)
+export const getFiles = () => fetch(`${appBase}file`)
+export const postEmp = (data) => fetch(`${appBase}add_emp`, {
+    method: 'POST',
+    body: JSON.stringify(data)
+})
+export const postFile = (data) => fetch(`${appBase}add_file`, {
+    method: 'POST',
+    body: data
+})
